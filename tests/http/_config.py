08155f5fda81fd4a8ba1eb6adb8f13c73255109d
Jaeho Cho <jaeho2025@gmail.com>
2025-04-30 02:41:05 -0400

    clean

--- tests/http/_config.py
+++
@@ -1,9 +0,0 @@
-from aiortc import RTCIceServer
-
-TURN = RTCIceServer(
-    urls="turn:24.193.235.114:3478",
-    username="jaeho",
-    credential="2121"
-)
-
-SIGNALING_URL = "http://24.193.235.114:8080"  # Replace with your signaling server IP
