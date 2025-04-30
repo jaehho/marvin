8f23184468c853d1cf0332dbb3bf2074e7a6b322
Jaeho Cho <jaeho2025@gmail.com>
2025-04-30 02:39:36 -0400

    rm about.vue

--- app/app.vue
+++ app/app.vue
@@ -8,9 +8,6 @@
           <NuxtLink to="/">Home</NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-        <li>
           <NuxtLink to="/control">Control</NuxtLink>
         </li>
         <li>
